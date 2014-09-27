b5c3c044edc93f9855cab03e3126e49787b3c58f
Mark Pine <mark.pine@icainformatics.com>
2014-09-27 01:51:44 -0500

    Deleted unnecessary projects;

--- EscapeMobility.Domain/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EscapeMobility.Domain
-{
-    public class Category
-    {
-        public virtual int CategoryId { get; set; }
-        public virtual string CategoryName { get; set; }
-        public virtual string CategoryDescription { get; set; }
-    }
-}
